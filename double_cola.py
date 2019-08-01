5363a188a8d838118ceaf18bb9dc5236c14a4ed5
Saidakbar Pardaev <saidakbarp@gmail.com>
2019-08-01 08:22:46 -0700

    level: 5 kyu

--- double_cola.py
+++ double_cola.py
@@ -1,5 +1,5 @@
 """
-level:  kyu
+level: 5 kyu
 Link: https://www.codewars.com/kata/double-cola/train/python
 Description:
 Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on.
